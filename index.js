7596a4a20764e5de1e6d2bfb59d1114ddb0cb21c
wooorm <tituswormer@gmail.com>
2014-10-19 16:25:01 +0200

    Refactor module

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 'use strict';
 
-var Interface,
-    data;
+/**
+ * Dependencies.
+ */
+
+var Interface;
 
 Interface = require('datamap-interface');
+
+/**
+ * Data.
+ */
+
+var data;
+
 data = require('./data/iso-639-3.json');
+
+/**
+ * Expose iso-639-3.
+ */
 
 module.exports = new Interface(data);
