ee4a803fe98e05c1c08dc4577600207e2b003bf5
Titus Wormer <tituswormer@gmail.com>
2017-07-25 14:22:42 -0400

    Refactor `build.js`

--- build.js
+++ build.js
@@ -6,12 +6,7 @@ var http = require('http');
 var concat = require('concat-stream');
 var unzip = require('unzip');
 var dsv = require('d3-dsv');
-
-var ID = '20160115';
-var INPUT = 'http://www-01.sil.org/iso639-3/' +
-    'iso-639-3_Code_Tables_' + ID + '.zip';
-var ENTRY = 'iso-639-3_' + ID + '.tab';
-var OUTPUT = path.join(__dirname, 'index.json');
+var bail = require('bail');
 
 var SCOPES = {
   I: 'individual',
@@ -28,28 +23,37 @@ var TYPES = {
   S: 'special'
 };
 
-http.request(INPUT, function (response) {
-  response.pipe(new unzip.Parse()).on('entry', function (entry) {
-    if (path.basename(entry.path) !== ENTRY) {
-      entry.autodrain();
-      return;
-    }
+http
+  .request('http://www-01.sil.org/iso639-3/iso-639-3_Code_Tables_20160115.zip', onrequest)
+  .end();
 
-    entry.pipe(concat(function (body) {
-      var data = dsv.tsvParse(body.toString()).map(function (language) {
-        return {
-          name: language.Ref_Name || null,
-          type: TYPES[language.Language_Type],
-          scope: SCOPES[language.Scope],
-          iso6393: language['﻿Id'], // There’s a `<U+FEFF>`
-          // in there, I don’t know why, but meh.
-          iso6392B: language.Part2B || null,
-          iso6392T: language.Part2T || null,
-          iso6391: language.Part1 || null
-        };
-      });
+function onrequest(response) {
+  response
+    .pipe(new unzip.Parse())
+    .on('entry', onentry);
+}
 
-      fs.writeFile(OUTPUT, JSON.stringify(data, 0, 2) + '\n');
-    }));
+function onentry(entry) {
+  if (path.basename(entry.path) === 'iso-639-3_20160115.tab') {
+    entry.pipe(concat(onconcat));
+  } else {
+    entry.autodrain();
+  }
+}
+
+function onconcat(body) {
+  var data = dsv.tsvParse(body.toString()).map(function (language) {
+    return {
+      name: language.Ref_Name || null,
+      type: TYPES[language.Language_Type],
+      scope: SCOPES[language.Scope],
+      iso6393: language['﻿Id'], // There’s a `<U+FEFF>`
+      // in there, I don’t know why, but meh.
+      iso6392B: language.Part2B || null,
+      iso6392T: language.Part2T || null,
+      iso6391: language.Part1 || null
+    };
   });
-}).end();
+
+  fs.writeFile('index.json', JSON.stringify(data, 0, 2) + '\n', bail);
+}
